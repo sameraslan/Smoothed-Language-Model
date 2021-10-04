12202264389e9270f4951b928cd6d9c4a085cfb1
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-10-04 17:32:27 -0400

    Q2 done pretty much

--- code/textcat.py
+++ code/textcat.py
@@ -51,14 +51,14 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def file_log_prob(file: Path, lm: LanguageModel) -> float:
+def file_log_prob(file: Path, lm: LanguageModel, prior_prob: float) -> float:
     """The file contains one sentence per line. Return the total
     log-probability of all these sentences, under the given language model.
     (This is a natural log, as for all our internal computations.)
     """
     log_prob = 0.0
     for (x, y, z) in read_trigrams(file, lm.vocab):
-        prob = lm.prob(x, y, z)  # p(z | xy)
+        prob = lm.prob(x, y, z) * prior_prob  # p(z | xy)
         log_prob += math.log(prob)
     return log_prob
 
@@ -75,6 +75,8 @@ def main():
     # But we'd like to print a value in bits: so we convert
     # log base e to log base 2 at print time, by dividing by log(2).
 
+    if (lm2.vocab != lm1.vocab):
+        log.error("Models need the same vocab file")
     log.info("Per-file log-probabilities:")
     total_log_prob1 = 0.0
     total_log_prob2 = 0.0
@@ -83,8 +85,8 @@ def main():
     total_count = 0
 
     for file in args.test_files:
-        log_prob1: float = file_log_prob(file, lm1)
-        log_prob2: float = file_log_prob(file, lm2)
+        log_prob1: float = file_log_prob(file, lm1, args.prior_prob)
+        log_prob2: float = file_log_prob(file, lm2, (1 - args.prior_prob))
         #print(f"{log_prob:g}\t{file}")
         if log_prob1 > log_prob2:
             print('gen',' ', file) #generalize for other category names
