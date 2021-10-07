3dc91aadd9ea2413d0510a8fbf7a6edd8254363f
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-10-07 19:24:20 -0400

    Changes

--- code/fileprob.py
+++ code/fileprob.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 import math
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 from probs import LanguageModel, num_tokens, read_trigrams
 
@@ -67,19 +68,27 @@ def main():
     log.info("Per-file log-probabilities:")
     total_log_prob = 0.0
     num_words_list = []
+    total_probs_list = []
     for file in args.test_files:
         file_name = file.stem
         file_num_words = file_name.split('.')[1]
-        num_words_list.append(file_num_words)
-        print(file_num_words)
+        num_words_list.append(float(file_num_words))
         log_prob: float = file_log_prob(file, lm)
+        total_probs_list.append(float(log_prob)/float(file_num_words))
         print(f"{log_prob:g}\t{file}")
         total_log_prob += log_prob
 
-    devbits = 87482
+    #devbits = 87482
     bits = -total_log_prob / math.log(2)   # convert to bits of surprisal
     tokens = sum(num_tokens(test_file) for test_file in args.test_files)
     print(f"Overall cross-entropy:\t{bits / tokens:.5f} bits per token")
+
+    plt.plot(num_words_list, total_probs_list, 'rx')
+    plt.xlabel("Number of words in file")
+    plt.ylabel("Lob Probability / Number of words")
+    plt.title("Gen Files Number of Words vs Log Probability")
+    plt.show()
+
     #print(f"Dev cross entropy:\t{bits / devbits:.5f} bits per token") #used to calculate dev bits for 3e
 
 
